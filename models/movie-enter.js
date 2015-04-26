91e803d8e1c84643e2b759144a6c6c87e137cd69
jlsears <jen.lauren.sears@gmail.com>
2015-04-26 18:26:11 -0500

    initial coding

---
+++ models/movie-enter.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+
+var movieSchema = mongoose.Schema({
+    title: {type: String, required: true, default: ''},
+    director: {type: String, required: false, default: ''},
+    genre: {type: String, required: false, default: '' },
+    theater: {type: String, required: false, default: '' },
+    fellow_moviegoers: {type: String, required: false, default: '' },
+    date_viewed: {type: Date, required: false, default: Date.now },
+    seen: {type: Dropdown, required: true, default: Date.now },
+    favorite: {type: Boolean, required: true, default: false },
+    user: {type: String, required: true}
+});
+
+var TheMovie = mongoose.model('TheMovie', movieSchema);
+
+module.exports = TheMovie;
