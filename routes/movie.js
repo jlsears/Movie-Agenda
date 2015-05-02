0b91c723b932bcb53b2e935c2d88e888b4f96c3a
jlsears <jen.lauren.sears@gmail.com>
2015-05-02 10:46:24 -0500

    Form page rendering

--- routes/movie.js
+++ routes/movie.js
@@ -38,6 +38,7 @@ var sendMovieList = function (req, res, next) {
 };
 
 
+
 // C. Handle a GET request from the client to /movieenter/list
 router.get('/list', function (req,res,next) {
   // Is the user logged in?
@@ -46,6 +47,86 @@ router.get('/list', function (req,res,next) {
   }
 
   sendMovieList(req, res, next);
+});
+
+
+// E. Handle a GET request from the client to /movieenter
+router.get('/', function (req, res) {
+
+  // Is the user logged in?
+  if (UserController.getCurrentUser() === null) {
+    res.redirect("/");
+  }
+
+  // Send the todo form back to the client
+  res.render('movieenter', {
+    title: 'Title Here',
+    themovie: {
+      title: '',
+      director: '',
+      genre: '',
+      theater: '',
+      fellow_moviegoers: '',
+      date_viewed: new Date(),
+      seen: '',
+      favorite: false
+    }
+  })
+})
+
+// G. Handle a POST request from the client to /movieenter
+router.post('/', function (req, res, next) {
+
+  // User is editing an existing item
+  if (req.body.db_id !== "") {
+
+    // Find it
+    movieenter.findOne({ _id: req.body.db_id }, function (err, foundmovieenter) {
+
+      if (err) {
+        sendError(req, res, err, "Could not find that task");
+      } else {
+        // Found it. Now update the values based on the form POST data.
+        foundmovieenter.title = req.body.title;
+        foundmovieenter.description = req.body.description;
+        foundmovieenter.priority = req.body.priority;
+        foundmovieenter.due_date = req.body.due_date;
+        foundmovieenter.complete = (req.body.complete) ? req.body.complete : false;
+
+        // Save the updated item.
+        foundmovieenter.save(function (err, newOne) {
+          if (err) {
+            sendError(req, res, err, "Could not save task with updated information");
+          } else {
+            res.redirect('/movie');
+          }
+        });
+      }
+    });
+
+  // User created a new item
+  } else {
+
+    // Who is the user?
+    var theUser = UserController.getCurrentUser();
+
+    // What did the user enter in the form?
+    var theFormPostData = req.body
+    theFormPostData.user = theUser._id;
+
+    console.log('theFormPostData',theFormPostData);
+
+
+    var mytodo = new Todo(theFormPostData);
+
+    mymovieenter.save(function (err, todo) {
+      if (err) {
+        sendError(req, res, err, "Failed to save task");
+      } else {
+        res.redirect('/movie');
+      }
+    });
+  }
 });
 
 
